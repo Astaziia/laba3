e7777d65fed5849245664aed9dc52bb090600a88
student <student@WS15-407>
2026-01-21 16:35:26 +0500

    Color

--- GraphObject.cs
+++ GraphObject.cs
@@ -17,7 +17,7 @@ namespace laba_2
 
         protected GraphObject()
         {
-            Color[] cols = { Color.Pink, Color.DeepPink, Color.Violet};
+            Color[] cols = { Color.Pink, Color.DeepPink, Color.Violet, color.HotPink};
             color = cols[r.Next(cols.Length)];
             brush = new SolidBrush(color);
 
